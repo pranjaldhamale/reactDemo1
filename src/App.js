40ac5b3d2c0b2a402bedb109554af33eef485ee3
Pranjal <pranjal dhamale>
2023-05-10 16:39:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> demo git react </h1>
+      <p>
+        hello how are you
+      </p>
+    </div>
+  );
+}
+
+export default App;
